a8ce8b00c7a615de80e8c988376548af4653f7a8
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-09 17:07:24 +0300

    make build

---
+++ index.b55f179d.js
@@ -0,0 +1,2 @@
+document.querySelector(".header__title").style.animationPlayState="running";
+//# sourceMappingURL=index.b55f179d.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,5 @@
+"use strict";
+const mainTitle = document.querySelector(".header__title");
+mainTitle.style.animationPlayState = "running";
+
+//# sourceMappingURL=index.f75de5e1.js.map
